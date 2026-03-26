18a0568266a46a674ba36b3aac0e310007b115c9
JoliCollectiveGH <info@jolicollective.net>
2026-03-26 11:49:37 +0000

    fix: remove www redirect from code, handled by Vercel

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,6 @@ const nextConfig: NextConfig = {
   productionBrowserSourceMaps: false,
   compress: true,
   poweredByHeader: false,
-  async redirects() {
-    return [
-      {
-        source: '/((?!_next|favicon|og-image|.*\\..*).*)',
-        has: [{ type: 'host', value: 'www.jolicollective.net' }],
-        destination: 'https://jolicollective.net/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
